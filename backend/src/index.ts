0680e8dd2cb23a6908557d7269297cd624496396
Tim Perry <pimterry@gmail.com>
2021-12-10 16:24:20 +0100

    Wait for worker schedule update to complete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import { checkServiceStatuses } from './check-status';
 import { getServiceStatus as getServiceStatuses } from './get-statuses';
 
-addEventListener('scheduled', () => {
-  checkServiceStatuses();
+addEventListener('scheduled', (event: any) => {
+  event.waitUntil(checkServiceStatuses());
 });
 
 addEventListener('fetch', (event: any) => {
